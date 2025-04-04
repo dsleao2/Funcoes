e7e2fdd1b014b5e523d7ed04772aaddff7b7bc3d
Daniele Santana <dssleao2@icloud.com>
2025-04-04 15:12:42 -0300

    Função com  parâmetro
    Para diminuir a quantidade de linhas e funções que são parecidas, usamos as funções com parâmetros.

---
+++ app.js
@@ -0,0 +1,16 @@
+
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do número secreto';
+
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+
+
+functionexibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
