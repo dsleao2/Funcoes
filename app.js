74831fe8c14375b351c41bc339560580ebee0fa9
Daniele Santana <dssleao2@icloud.com>
2025-04-04 15:40:21 -0300

    Função com retorno/ Tipos de Funções
    Nesta atualização, especifiquei quais funções são com parâmetro, retorno e a que não tem nenhum dos dois.

--- app.js
+++ app.js
@@ -5,12 +5,27 @@
 // let paragrafo = document.querySelector('p');
 // paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 
+let numerosecreto = gerarNumeroAleatorio ();
 
-
-functionexibirTextoNaTela(tag, texto){
+// função com parâmetro
+function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+
+// função que não tem parâmetro e não tem retorno 
+function verificarChute() {
+    console.log (numerosecreto);
+    
+}
+
+// função com retorno
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random () * 10 + 1);
+   
+
+}
